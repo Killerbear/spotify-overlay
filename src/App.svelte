db57025f938a803d37515cce63a35819b201d38a
Killerbear <killerbear_02@gmx.de>
2023-02-05 11:23:57 +0100

    add spotify api

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,54 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+	import { SpotifyService } from "./lib/services/spotifyService";
+
+	let item;
+
+	onMount(async () => {
+		let spotifyService = new SpotifyService();
+		const data = await spotifyService.getSong();
+		item = data.item;
+	});
 </script>
 
-<main />
+<main>
+	{#if item?.album}
+		<img src={item.album.images[0].url} alt="cover" />
+		<div class="info">
+			<div class="artist">{item?.artists[0]?.name}</div>
+			<div class="title">{item?.name}</div>
+		</div>
+	{/if}
+</main>
 
 <style>
+	main {
+		display: flex;
+	}
+
+	img {
+		width: 112px;
+		margin: 32px;
+	}
+
+	.info {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		margin: 32px 0;
+	}
+
+	.artist {
+		font-size: 28px;
+		font-weight: 500;
+		line-height: 32px;
+		color: #fff;
+	}
+
+	.title {
+		font-size: 22px;
+		font-weight: 400;
+		line-height: 32px;
+		color: #b3b3b3;
+	}
 </style>
---
+++ src/lib/services/spotifyService.ts
@@ -0,0 +1,56 @@
+class SpotifyService {
+	private token =
+		"BQDp7qLXnqy6qDgpxUH_TaOqV8drrrpGAiStP4awFaH2d5fpeWcw4xlvV318E6XBcHWxZivBjtrPjgh2nbgRYnQUmj256FO8ZCZwwbu6TD2IiptAy7pRmTJo7q6B1mWXNC-FATiOZPT4wcdxP46fZ4JzRdxc9oRLIWLkRSFYf4raW7kX5zgkQem8zw";
+
+	public async getSong() {
+		const response = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + this.token,
+			},
+		});
+		return await response.json();
+	}
+
+	// const client_id = "4646bc101b3345f59a834a3c180e57eb";
+	// const client_secret = "d331fc124b9b43809fbfa974499fee30";
+	// const redirect_uri = "http://localhost:8888/callback";
+	// const scopes = "user-read-currently-playing";
+
+	// const encode = (data) => {
+	// 	return Object.keys(data)
+	// 		.map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+	// 		.join("&");
+	// };
+	//
+	// const generateRandomString = function (length) {
+	// 	let text = "";
+	// 	let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	//
+	// 	for (let i = 0; i < length; i++) {
+	// 		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	// 	}
+	// 	return text;
+	// };
+	//
+	// const getAuthorization = async () => {
+	// 	let state = generateRandomString(16);
+	// };
+	//
+	// const getAuthToken = (async () => {
+	// 	const response = await fetch("https://accounts.spotify.com/api/token", {
+	// 		method: "POST",
+	// 		headers: {
+	// 			Accept: "application/json",
+	// 			"Content-Type": "application/x-www-form-urlencoded",
+	// 			Authorization: "Basic " + btoa(client_id + ":" + client_secret),
+	// 		},
+	// 		body: encode({ grant_type: "client_credentials" }),
+	// 	});
+	// 	return await response.json();
+	// })();
+}
+
+export { SpotifyService };
